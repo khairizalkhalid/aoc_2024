17d357f6ed8583a063d8b874cdd658eca0c231a6
khairizalkhalid <khairizalkhalid@outlook.com>
2024-12-08 00:51:32 +0800

    refactor read file implementation to its own feature.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
 use std::fs::File;
 use std::io::{self, Read};
 
-fn main() -> io::Result<()> {
-    println!("Hello, world!");
-
-    let mut file = File::open("input/day1.txt")?;
+fn read_file() -> io::Result<String> {
+    let mut file = File::open("input/day1x.txt")?;
     let mut contents = String::new();
 
     file.read_to_string(&mut contents)?;
-    print!("{}", contents);
-    Ok(())
+    return Ok(contents);
+}
+
+fn main() {
+    println!("Hello, world!");
+
+    let contents = read_file();
+
+    match contents {
+        Ok(contents) => println!("{}", contents),
+        Err(e) => println!("Error: {}", e),
+    }
 }
