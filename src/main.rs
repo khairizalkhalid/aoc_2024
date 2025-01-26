9b8c9edcae2259493fc203103e3530154fbcf084
khairizalkhalid <khairizalkhalid@outlook.com>
2025-01-26 21:32:33 +0800

    add \n for each print to properly display without the trailing % sign.

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,13 @@ mod days;
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
-        eprint!("Please provide which day and part to run. (e.g: `cargo run d1p1`)");
+        eprint!("Please provide which day and part to run. (e.g: `cargo run d1p1`)\n");
         return;
     }
 
     let day = &args[1];
     match day.as_str() {
         "d1p1" => days::day1_part1::run(),
-        _ => eprint!("Program {} is not available.", day),
+        _ => eprint!("Program {} is not available.\n", day),
     }
 }
