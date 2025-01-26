504631cf187f1805abc66536bfd63b2be61851a8
khairizalkhalid <khairizalkhalid@outlook.com>
2025-01-26 20:58:40 +0800

    enhance main.rs to call which number of day module with argument.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
+use std::env;
 
+mod days;
 
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprint!("Please provide which day to run. (e.g: `cargo run 1`)");
+        return;
+    }
 
+    let day = &args[1];
+    match day.as_str() {
+        "1" => days::day1::run(),
+        _ => eprint!("Day {} is not available.", day),
+    }
 }
