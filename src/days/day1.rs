ced9ca05a4dd8b5736547c5f116d6b82eff419e3
khairizalkhalid <khairizalkhalid@outlook.com>
2025-01-26 20:57:58 +0800

    move day1 task into it's own module.

---
+++ src/days/day1.rs
@@ -0,0 +1,90 @@
+use std::fs::File;
+use std::i32;
+use std::io::{self, Read};
+use std::time::Instant;
+
+fn read_file() -> io::Result<String> {
+    let mut file = File::open("./input/day1.txt")?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    Ok(contents)
+}
+
+fn split_contents_into_two_vectors(contents: &str) -> (Vec<i32>, Vec<i32>) {
+    let mut vec1 = Vec::new();
+    let mut vec2 = Vec::new();
+
+    for line in contents.lines() {
+        let items: Vec<&str> = line.split_whitespace().collect();
+        if items.len() == 2 {
+            if let Ok(item1) = items[0].parse::<i32>() {
+                vec1.push(item1);
+            }
+            if let Ok(item2) = items[1].parse::<i32>() {
+                vec2.push(item2);
+            }
+        }
+    }
+
+    (vec1, vec2)
+}
+
+// My own implementation to buble sort. Improved sorting should be using the rust default sort.
+#[allow(dead_code)]
+fn bubble_sort(mut vec: Vec<i32>) -> Vec<i32> {
+    let max = vec.len();
+    let mut temp: i32;
+    let mut swapped: bool;
+    for i in 0..max - 1 {
+        swapped = false;
+        for j in 0..max - i - 1 {
+            if vec[j] > vec[j + 1] {
+                temp = vec[j];
+                vec[j] = vec[j + 1];
+                vec[j + 1] = temp;
+                swapped = true;
+            }
+        }
+        if !swapped {
+            break;
+        }
+    }
+    vec
+}
+
+fn calculate_distance_of_two_vectors(vec1: Vec<i32>, vec2: Vec<i32>) -> Vec<i32> {
+    vec1.iter()
+        .zip(vec2.iter())
+        .map(|(a, b)| (a - b).abs())
+        .collect()
+}
+
+// my implementation to sum in a vector. Improved version should be using rust iter sum
+#[allow(dead_code)]
+fn sum_of_vector(vec: Vec<i32>) -> i32 {
+    let mut total: i32 = 0;
+    let max = vec.len();
+    for i in 0..max {
+        total += vec[i]
+    }
+    total
+}
+
+pub fn run() {
+    match read_file() {
+        Ok(contents) => {
+            let start = Instant::now();
+
+            let (mut vec1, mut vec2) = split_contents_into_two_vectors(&contents);
+            vec1.sort();
+            vec2.sort();
+            let distance_vec = calculate_distance_of_two_vectors(vec1, vec2);
+            let total_distance: i32 = distance_vec.iter().sum();
+            println!("{}", total_distance);
+
+            let duration = start.elapsed();
+            println!("Time elapsed: {:?}", duration);
+        }
+        Err(e) => println!("Error: {}", e),
+    }
+}
