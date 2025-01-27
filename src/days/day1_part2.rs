3c0cfb8412176584c487fc0c71e78e3fb7576145
khairizalkhalid <khairizalkhalid@outlook.com>
2025-01-28 00:03:27 +0800

    implment day1 part2.

--- src/days/day1_part2.rs
+++ src/days/day1_part2.rs
@@ -1,3 +1,6 @@
+use crate::days::day1_part1;
+use std::i32;
+
 pub fn run() {
     // first index: iterate thru to find equal number (call it cur)
     // found, add to counter
@@ -5,4 +8,18 @@ pub fn run() {
     // replace cur with score
     // repeate for each vec1...
     // the end sum all vec1
+    match day1_part1::read_file() {
+        Ok(contents) => {
+            let mut final_vec: Vec<i32> = Vec::new();
+            let (vec1, vec2) = day1_part1::split_contents_into_two_vectors(&contents);
+            for cur in vec1 {
+                let count = vec2.iter().filter(|a| **a == cur).count() as i32;
+                final_vec.push(cur * count)
+            }
+
+            let total: i32 = final_vec.iter().sum();
+            println!("{:?}", total)
+        }
+        Err(e) => println!("Error: {}", e),
+    }
 }
