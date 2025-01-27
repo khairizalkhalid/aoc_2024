9f027b9846d7077abc19402664c6b3bee1d6f4ec
khairizalkhalid <khairizalkhalid@outlook.com>
2025-01-27 23:46:25 +0800

    use println instead of adding that newline in the string itself lol.

--- src/days/day1_part2.rs
+++ src/days/day1_part2.rs
@@ -1,3 +1,3 @@
 pub fn run() {
-    print!("monkaHmm\n");
+    println!("monkaHmm");
 }
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ mod days;
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
-        eprint!("Please provide which day and part to run. (e.g: `cargo run d1p1`)\n");
+        eprintln!("Please provide which day and part to run. (e.g: `cargo run d1p1`)");
         return;
     }
 
@@ -13,6 +13,6 @@ fn main() {
     match day.as_str() {
         "d1p1" => days::day1_part1::run(),
         "d1p2" => days::day1_part2::run(),
-        _ => eprint!("Program {} is not available.\n", day),
+        _ => eprintln!("Program {} is not available.", day),
     }
 }
