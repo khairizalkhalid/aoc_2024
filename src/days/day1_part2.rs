ed8853176f26cc8da05306e492a8dd66f8bddaaa
khairizalkhalid <khairizalkhalid@outlook.com>
2025-01-27 23:59:16 +0800

    add pseudocode for day 1 part 2.

--- src/days/day1_part2.rs
+++ src/days/day1_part2.rs
@@ -1,3 +1,8 @@
 pub fn run() {
-    println!("monkaHmm");
+    // first index: iterate thru to find equal number (call it cur)
+    // found, add to counter
+    // finally multiply cur with the counter call it score
+    // replace cur with score
+    // repeate for each vec1...
+    // the end sum all vec1
 }
