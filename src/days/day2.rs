f14f4e69bf56b5ec3f9862807674d2daf02b7b61
khairizalkhalid <khairizalkhalid@outlook.com>
2025-01-29 17:14:56 +0800

    move test run into a pub function.

--- src/days/day2.rs
+++ src/days/day2.rs
@@ -41,6 +41,24 @@ fn safety_check(readings: Vec<i32>) -> bool {
     safety == Safety::Safe
 }
 
+pub fn test_run() {
+    let test_case = vec![
+        vec![7, 6, 4, 2, 1], //Safe because the levels are all decreasing by 1 or 2.
+        vec![1, 2, 7, 8, 9], //Unsafe because 2 7 is an increase of 5.
+        vec![9, 7, 6, 2, 1], //Unsafe because 6 2 is a decrease of 4.
+        vec![1, 3, 2, 4, 5], //Unsafe because 1 3 is increasing but 3 2 is decreasing.
+        vec![8, 6, 4, 4, 1], //Unsafe because 4 4 is neither an increase or a decrease.
+        vec![1, 3, 6, 7, 9], //Safe because the levels are all increasing by 1, 2, or 3.
+    ];
+
+    let result: Vec<bool> = test_case
+        .iter()
+        .map(|vec_z| safety_check(vec_z.to_vec()))
+        .collect();
+    println!("{:?}", result);
+    println!("{:?}", result.iter().filter(|&&r| r).count());
+}
+
 pub fn run() {
     // convert data into a X of Y
     // repeat for each Y
@@ -57,19 +75,4 @@ pub fn run() {
     // - second loop: check (isIncreasing && safeRange < 0) || (!isIncreasing && safeRange > 0): return unsafe
     // - return safe
     // count safe in Z
-    let test_case = vec![
-        vec![7, 6, 4, 2, 1], //Safe because the levels are all decreasing by 1 or 2.
-        vec![1, 2, 7, 8, 9], //Unsafe because 2 7 is an increase of 5.
-        vec![9, 7, 6, 2, 1], //Unsafe because 6 2 is a decrease of 4.
-        vec![1, 3, 2, 4, 5], //Unsafe because 1 3 is increasing but 3 2 is decreasing.
-        vec![8, 6, 4, 4, 1], //Unsafe because 4 4 is neither an increase or a decrease.
-        vec![1, 3, 6, 7, 9], //Safe because the levels are all increasing by 1, 2, or 3.
-    ];
-
-    let result: Vec<bool> = test_case
-        .iter()
-        .map(|vec_z| safety_check(vec_z.to_vec()))
-        .collect();
-    println!("{:?}", result);
-    println!("{:?}", result.iter().filter(|&&r| r).count());
 }
