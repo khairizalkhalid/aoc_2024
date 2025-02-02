24654418822d8325f4f6ab2b9396918db2777e7a
khairizalkhalid <khairizalkhalid@outlook.com>
2025-02-02 22:42:14 +0800

    add regex crate and test sample run for regex.

--- src/days/day3_part1.rs
+++ src/days/day3_part1.rs
@@ -1,3 +1,5 @@
+use regex::Regex;
+
 pub fn test_run() {
     // split string by regex exp `mul\(\d+,\d+\)`
     // for each set of string
@@ -7,4 +9,14 @@ pub fn test_run() {
     // calculate multiplication between the two i32
     // push to a new vec
     // then iter sum it up.
+
+    let test_case = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+
+    let reg = Regex::new(r"mul\(\d+,\d+\)").unwrap();
+
+    let Some(caps) = reg.captures(test_case) else {
+        println!("not found");
+        return;
+    };
+    println!("{:?}", &caps)
 }
