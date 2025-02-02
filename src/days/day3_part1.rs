28a1cee7809b2ec9dc4fa83d34d8fd0f8db5d486
khairizalkhalid <khairizalkhalid@outlook.com>
2025-02-02 22:41:30 +0800

    add pseudocode for day 3 part 1.

---
+++ src/days/day3_part1.rs
@@ -0,0 +1,10 @@
+pub fn test_run() {
+    // split string by regex exp `mul\(\d+,\d+\)`
+    // for each set of string
+    // remove the word 'mul'
+    // split using comma, first item remove infront while the second remove the end
+    // parse to i32
+    // calculate multiplication between the two i32
+    // push to a new vec
+    // then iter sum it up.
+}
