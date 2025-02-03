0a6f8dfd1684a391057a3d0813cba67af336de5c
khairizalkhalid <khairizalkhalid@outlook.com>
2025-02-03 23:18:46 +0800

    separate test_run into their own fn.

--- src/days/day3_part1.rs
+++ src/days/day3_part1.rs
@@ -1,5 +1,25 @@
 use regex::Regex;
 
+fn sum_of_muls(mul_string: &str) -> i32 {
+    let reg = Regex::new(r"mul\(\d+,\d+\)").unwrap();
+    let reg_matches: Vec<_> = reg.find_iter(mul_string).map(|m| m.as_str()).collect();
+
+    let sum_of_products: i32 = reg_matches
+        .iter()
+        .filter_map(|m| {
+            m.strip_prefix("mul(")
+                .and_then(|s| s.strip_suffix(")"))
+                .map(|s| {
+                    s.split(',')
+                        .map(|n| n.parse::<i32>().unwrap())
+                        .product::<i32>()
+                })
+        })
+        .sum();
+
+    sum_of_products
+}
+
 pub fn test_run() {
     // split string by regex exp `mul\(\d+,\d+\)`
     // for each set of string
@@ -11,18 +31,5 @@ pub fn test_run() {
     // then iter sum it up.
 
     let test_case = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
-
-    let reg = Regex::new(r"mul\(\d+,\d+\)").unwrap();
-    let reg_matches: Vec<_> = reg.find_iter(test_case).map(|m| m.as_str()).collect();
-
-    let products: Vec<i32> = reg_matches
-        .iter()
-        .filter_map(|m| {
-            m.strip_prefix("mul(")
-                .and_then(|s| s.strip_suffix(")"))
-                .map(|s| s.split(',').map(|n| n.parse::<i32>().unwrap()).product())
-        })
-        .collect();
-
-    println!("{:?}", products)
+    println!("{:?}", sum_of_muls(test_case));
 }
