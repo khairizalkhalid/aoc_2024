7e1d30274a7354a353a882c011b0f141f4d0a2cd
khairizalkhalid <khairizalkhalid@outlook.com>
2025-02-03 23:10:33 +0800

    properly chain the strip suffix and prefix.

--- src/days/day3_part1.rs
+++ src/days/day3_part1.rs
@@ -15,22 +15,18 @@ pub fn test_run() {
     let reg = Regex::new(r"mul\(\d+,\d+\)").unwrap();
     let reg_matches: Vec<_> = reg.find_iter(test_case).map(|m| m.as_str()).collect();
 
-    println!("{:?}", reg_matches);
-
-    let strip_mul: Vec<Vec<i32>> = reg_matches
+    let numbers: Vec<Vec<i32>> = reg_matches
         .iter()
-        .map(|m| {
+        .filter_map(|m| {
             m.strip_prefix("mul(")
-                .get_or_insert_default()
-                .to_string()
-                .strip_suffix(")")
-                .get_or_insert_default()
-                .to_string()
-                .split(",")
-                .map(|n| n.parse::<i32>().unwrap())
-                .collect()
+                .and_then(|s| s.strip_suffix(")"))
+                .map(|s| {
+                    s.split(',')
+                        .map(|n| n.parse::<i32>().unwrap())
+                        .collect::<Vec<i32>>()
+                })
         })
         .collect();
 
-    println!("{:?}", strip_mul)
+    println!("{:?}", numbers)
 }
