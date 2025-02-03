fbe8a8143dad4151007aac5fe32d441cb04833fd
khairizalkhalid <khairizalkhalid@outlook.com>
2025-02-03 22:41:45 +0800

    implement regex matches and strip off mul(d,d) and convert it into Vec<Vec<i32>>

--- src/days/day3_part1.rs
+++ src/days/day3_part1.rs
@@ -13,10 +13,24 @@ pub fn test_run() {
     let test_case = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
 
     let reg = Regex::new(r"mul\(\d+,\d+\)").unwrap();
+    let reg_matches: Vec<_> = reg.find_iter(test_case).map(|m| m.as_str()).collect();
 
-    let Some(caps) = reg.captures(test_case) else {
-        println!("not found");
-        return;
-    };
-    println!("{:?}", &caps)
+    println!("{:?}", reg_matches);
+
+    let strip_mul: Vec<Vec<i32>> = reg_matches
+        .iter()
+        .map(|m| {
+            m.strip_prefix("mul(")
+                .get_or_insert_default()
+                .to_string()
+                .strip_suffix(")")
+                .get_or_insert_default()
+                .to_string()
+                .split(",")
+                .map(|n| n.parse::<i32>().unwrap())
+                .collect()
+        })
+        .collect();
+
+    println!("{:?}", strip_mul)
 }
--- src/main.rs
+++ src/main.rs
@@ -28,7 +28,7 @@ fn main() {
         "d2t" => time_elapsed_wrapper(|| days::day2_part1::test_run()),
         "d2p1" => time_elapsed_wrapper(|| days::day2_part1::run()),
         "d2p2" => time_elapsed_wrapper(|| days::day2_part2::run()),
-        "d3p1" => time_elapsed_wrapper(|| days::day3_part1::test_run()),
+        "d3pt" => time_elapsed_wrapper(|| days::day3_part1::test_run()),
         _ => eprintln!("Program {} is not available.", day),
     }
 }
