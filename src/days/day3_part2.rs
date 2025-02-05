ad12e473723c637971532acc1723c2c18d77fc35
khairizalkhalid <khairizalkhalid@outlook.com>
2025-02-05 21:43:03 +0800

    complete the implementation of day 3 part 2.

--- src/days/day3_part2.rs
+++ src/days/day3_part2.rs
@@ -4,9 +4,13 @@ use super::day3_part1::sum_of_muls;
 
 fn sum_of_doos(contents: &str) -> i32 {
     let mut doos: Vec<&str> = Vec::new();
-    let splited: Vec<_> = contents.split("don't()").collect();
-    doos.push(splited.get(0).unwrap());
-    println!("{:?}", doos);
+    let splitted: Vec<&str> = contents.split("don't()").collect();
+    doos.push(splitted.get(0).unwrap());
+
+    for part in splitted.iter().skip(1) {
+        doos.extend(part.split("do()").skip(1));
+    }
+
     sum_of_muls(doos.join("").as_str())
 }
 
@@ -15,10 +19,8 @@ pub fn run() {
     // start capture until found do_not
     // continue capture again after matched do
     // send this string unto sum_of_muls
-    let test_case = "xmul(2,4)%&mul[3,7]!@^don't()_mul(5,5)+mul(32,64]then(mul(11,8)do()mul(8,5))";
-    println!("{:?}", sum_of_doos(test_case));
-    //match utils::file_reader::read_file("day3.txt") {
-    //    Ok(contents) => println!("{:?}", sum_of_doos(&contents)),
-    //    Err(e) => println!("Err: {}", e),
-    //}
+    match utils::file_reader::read_file("day3.txt") {
+        Ok(contents) => println!("{:?}", sum_of_doos(&contents)),
+        Err(e) => println!("Err: {}", e),
+    }
 }
