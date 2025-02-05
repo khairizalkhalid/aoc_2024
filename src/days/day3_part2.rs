22b39272ba101a856509d820cddec9a948fe3ad4
khairizalkhalid <khairizalkhalid@outlook.com>
2025-02-05 21:53:29 +0800

    fix the comment pseudocode

--- src/days/day3_part2.rs
+++ src/days/day3_part2.rs
@@ -15,10 +15,11 @@ fn sum_of_doos(contents: &str) -> i32 {
 }
 
 pub fn run() {
-    // from part 1 filter for do until do_not
-    // start capture until found do_not
-    // continue capture again after matched do
-    // send this string unto sum_of_muls
+    // from part 1 filter "do" "don't" with:
+    // split "dont", push the first index into do vec
+    // repeat the rest of the vec to split "do"
+    // skip first index and push the rest into do vec
+    // send this do vec unto sum_of_muls
     match utils::file_reader::read_file("day3.txt") {
         Ok(contents) => println!("{:?}", sum_of_doos(&contents)),
         Err(e) => println!("Err: {}", e),
