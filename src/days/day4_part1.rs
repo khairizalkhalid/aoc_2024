08557c27a86dbbb573237445576b12492b455932
khairizalkhalid <khairizalkhalid@outlook.com>
2025-02-07 21:44:08 +0800

    add char iter function to call count match set of word.

--- src/days/day4_part1.rs
+++ src/days/day4_part1.rs
@@ -1,3 +1,23 @@
+const TARGET_WORD: &str = "XMAS";
+
+fn count_word(target_word: &str, input_vec: &Vec<Vec<char>>) -> i32 {
+    let target_chars: Vec<char> = target_word.chars().collect();
+
+    let count_vec: Vec<i32> = input_vec
+        .iter()
+        .map(|y| {
+            y.iter()
+                .map(|x| count_match_chars(target_word, input_vec))
+                .sum()
+        })
+        .collect();
+    count_vec.iter().sum()
+}
+
+fn count_match_chars(target_word: &str, input_vec: &Vec<Vec<char>>) -> i32 {
+    1
+}
+
 pub fn test_run() {
     let test_str = "MMMSXXMASM
 MSAMXMSMSA
@@ -10,7 +30,14 @@ SAXAMASAAA
 MAMMMXMMMM
 MXMXAXMASX";
 
-    let mut coordinate: Vec<Vec<char>> = test_str.lines().map(|l| l.chars().collect()).collect();
+    let mut str_coordinate: Vec<Vec<char>> =
+        test_str.lines().map(|l| l.chars().collect()).collect();
 
-    println!("test {:?}", coordinate)
+    println!("test {:?}", str_coordinate);
+
+    println!(
+        "{} word count: {}",
+        TARGET_WORD,
+        count_word(TARGET_WORD, &str_coordinate)
+    );
 }
