69f1d47f2643681007f07dda4e3dfa16054b522e
khairizalkhalid <khairizalkhalid@outlook.com>
2025-02-11 00:31:45 +0800

    optimize day4 part2, combine repeating function.

--- src/days/day4_part2.rs
+++ src/days/day4_part2.rs
@@ -29,56 +29,42 @@ fn is_match_x(target_chars: &[char], root_index: (usize, usize), input_vec: &[Ve
         (1, -1), // northeast
     ];
 
-    let mut backslash_match = true;
-    let mut slash_match = true;
-
-    for (dx, dy) in backslash_directions.iter() {
-        for (i, &target_char) in target_chars.iter().enumerate() {
-            let new_x = x_i as isize - (1 * dx) + i as isize * dx;
-            let new_y = y_i as isize - (1 * dy) + i as isize * dy;
-            if new_x < 0
-                || new_y < 0
-                || new_x >= input_vec[0].len() as isize
-                || new_y >= input_vec.len() as isize
-            {
-                backslash_match = false;
-                break;
-            }
-            if input_vec[new_y as usize][new_x as usize] != target_char {
-                backslash_match = false;
-                break;
-            }
-            backslash_match = true;
-        }
-        if backslash_match {
-            break;
-        }
-    }
-
-    for (dx, dy) in slash_directions.iter() {
-        for (i, &target_char) in target_chars.iter().enumerate() {
-            let new_x = x_i as isize - (1 * dx) + i as isize * dx;
-            let new_y = y_i as isize - (1 * dy) + i as isize * dy;
-            if new_x < 0
-                || new_y < 0
-                || new_x >= input_vec[0].len() as isize
-                || new_y >= input_vec.len() as isize
-            {
-                slash_match = false;
-                break;
-            }
-            if input_vec[new_y as usize][new_x as usize] != target_char {
-                slash_match = false;
-                break;
-            }
-            slash_match = true;
-        }
-        if slash_match {
-            break;
-        }
-    }
+    let backslash_match = is_half_cross(&backslash_directions, target_chars, (x_i, y_i), input_vec);
+    let slash_match = is_half_cross(&slash_directions, target_chars, (x_i, y_i), input_vec);
 
     backslash_match && slash_match
+}
+
+fn is_half_cross(
+    directions: &[(isize, isize)],
+    target_chars: &[char],
+    (x_i, y_i): (usize, usize),
+    input_vec: &[Vec<char>],
+) -> bool {
+    let mut half_cross = false;
+    for (dx, dy) in directions.iter() {
+        for (i, &target_char) in target_chars.iter().enumerate() {
+            let new_x = x_i as isize - (1 * dx) + i as isize * dx;
+            let new_y = y_i as isize - (1 * dy) + i as isize * dy;
+            if new_x < 0
+                || new_y < 0
+                || new_x >= input_vec[0].len() as isize
+                || new_y >= input_vec.len() as isize
+            {
+                half_cross = false;
+                break;
+            }
+            if input_vec[new_y as usize][new_x as usize] != target_char {
+                half_cross = false;
+                break;
+            }
+            half_cross = true;
+        }
+        if half_cross {
+            break;
+        }
+    }
+    half_cross
 }
 
 #[allow(dead_code)]
