e2297129e5c824a1f3fb0107f15017aa58ed2689
khairizalkhalid <khairizalkhalid@outlook.com>
2025-03-01 23:31:04 +0800

    implement standard usage of easy to call and stdout the output.

--- src/utils/get_input_file.rs
+++ src/utils/get_input_file.rs
@@ -1,8 +1,21 @@
+use std::io::{stdout, Write};
+
+use curl::easy::Easy;
+
 // create a utility to download aoc input files and store in input folder as a txt file
 // if args size 2 and args[1] = 'get_input' ie: 'cargo run get_input 6
 // download file using curl into input/dayX.txt
 // `curl -v --cookie "session=AOC24_COOKIE" https://adventofcode.com/2024/day/7/input > input/day7.txt`
 // store session cookie in system environment AOC24_COOKIE
 pub fn run(day: &str) {
-    println!("this is sparta {}", day)
+    let mut easy = Easy::new();
+    let url = "https://adventofcode.com/2024/day/".to_string() + day + "/input";
+
+    easy.url(&url).unwrap();
+    easy.write_function(|data| {
+        stdout().write_all(data).unwrap();
+        Ok(data.len())
+    })
+    .unwrap();
+    easy.perform().unwrap()
 }
