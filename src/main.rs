deb5a69497a66f38b9cdc51a6f1da11d987d5e83
khairizalkhalid <khairizalkhalid@outlook.com>
2025-03-01 23:29:39 +0800

    enhance the main to specifically check "input" in args as the identifier to go into the get_input_file fn.

--- src/main.rs
+++ src/main.rs
@@ -18,12 +18,15 @@ where
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
-        eprintln!("Please provide which day and part to run. (e.g: `cargo run d1p1`)");
+        eprintln!(
+            "Please provide which day and part to run. (e.g: `cargo run d1p1`, `cargo input 1`)"
+        );
         return;
     }
 
-    if args.len() == 3 {
+    if &args[1] == "input" && args.len() == 3 {
         utils::get_input_file::run(&args[2]);
+        return;
     }
 
     let day = &args[1];
