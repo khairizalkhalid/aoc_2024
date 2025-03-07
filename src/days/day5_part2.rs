f7e935c7acdfb1f295a5346a806b4d51a7576459
khairizalkhalid <khairizalkhalid@outlook.com>
2025-03-08 03:42:11 +0800

    also do rule tuple clone like part 1.

--- src/days/day5_part2.rs
+++ src/days/day5_part2.rs
@@ -111,6 +111,7 @@ mod test {
 47|29
 75|13
 53|13";
+        let rule_tuples = rule_to_tuple(rules);
 
         let pages = "75,47,61,53,29
 97,61,53,29,13
@@ -125,7 +126,7 @@ mod test {
             vec!["97", "13", "75", "29", "47"],
         ];
         assert_eq!(
-            get_pages_with_rules(pages, rule_to_tuple(rules), true),
+            get_pages_with_rules(pages, rule_tuples.clone(), true),
             expected
         );
 
@@ -134,7 +135,7 @@ mod test {
 75,29,13";
         let expected_no_match: Vec<Vec<&str>> = vec![];
         assert_eq!(
-            get_pages_with_rules(pages_match, rule_to_tuple(rules), true),
+            get_pages_with_rules(pages_match, rule_tuples.clone(), true),
             expected_no_match
         );
     }
