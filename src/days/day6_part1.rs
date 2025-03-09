1c1012c4abb1ce252f2572719fe5ea0f0c0a9286
khairizalkhalid <khairizalkhalid@outlook.com>
2025-03-09 08:59:36 +0800

    add pseudocode for day 6 part 1

--- src/days/day6_part1.rs
+++ src/days/day6_part1.rs
@@ -1,3 +1,12 @@
 pub fn run() {
     println!("Day6")
+    // convert the map into x y
+    // map store obsticle
+    // create guard tuple/obj w position, direction and steps taken
+    // check destination xy in front of direction
+    // where direction 0,90,180,270
+    // 0 = (0,1), 90 = (1,0), 180 = (-1,0), 270 = (0,-1)
+    // if out of bounds, end
+    // if obs update +90 direction
+    // else, count steps and update position
 }
