28aae52e26d19d355afd0ff2df992b0ca486103d
khairizalkhalid <khairizalkhalid@outlook.com>
2025-03-19 06:39:40 +0800

    add day6 part2 pseudocode.

---
+++ src/days/day6_part2.rs
@@ -0,0 +1,10 @@
+pub fn run() {
+    println!("Day Six Part Two.");
+    // for each of the mark visitted,
+    // add new obsticle 'O' in the canvas
+    // move the entity
+    // use new visitted marker '+/-/|' (now need to enhance with direction too)
+    // check for is_front_loop where it is checking for the visitted marker with entity direction
+    // if it is a loop, store this new obsticle xy else continue foreach mark visited
+    // finally count new obsticle created
+}
