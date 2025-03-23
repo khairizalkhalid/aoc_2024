f2faebd357376a142fa671c501d48c44addc0051
khairizalkhalid <khairizalkhalid@outlook.com>
2025-03-23 19:33:59 +0800

    add is_front_looping fn that actually is just an upgrade of is_front_clear fn.. hmm should have written better with how this 2 fn is similar.

--- src/days/day6_part2.rs
+++ src/days/day6_part2.rs
@@ -32,6 +32,46 @@ fn mark_visited_with_direction(
         .collect()
 }
 
+fn is_front_looping(entity_xy_dir: (i32, i32, i32), items: Vec<(i32, i32, char)>) -> bool {
+    let (ntt_x, ntt_y, ntt_dir) = entity_xy_dir;
+
+    match ntt_dir {
+        0 => {
+            let front_x = ntt_x;
+            let front_y = ntt_y - 1;
+            if items.contains(&(front_x, front_y, '|')) {
+                return true;
+            }
+            return false;
+        }
+        90 => {
+            let front_x = ntt_x + 1;
+            let front_y = ntt_y;
+            if items.contains(&(front_x, front_y, '-')) {
+                return true;
+            }
+            return false;
+        }
+        180 => {
+            let front_x = ntt_x;
+            let front_y = ntt_y + 1;
+            if items.contains(&(front_x, front_y, '|')) {
+                return true;
+            }
+            return false;
+        }
+        270 => {
+            let front_x = ntt_x - 1;
+            let front_y = ntt_y;
+            if items.contains(&(front_x, front_y, '-')) {
+                return true;
+            }
+            return false;
+        }
+        _ => false,
+    }
+}
+
 pub fn run() {
     println!("Day Six Part Two.");
     // for each of the mark visitted, add into a coordinate
@@ -101,5 +141,28 @@ mod test {
             mark_visited_with_direction(two_d_canvas.clone(), entity),
             expected
         );
+    }
+
+    #[test]
+    fn test_is_front_looping() {
+        let entity = (4, 1, 0);
+        let items = vec![(4, 0, '|'), (9, 1, '|')];
+
+        assert_eq!(is_front_looping(entity, items), true);
+
+        let entity = (4, 1, 90);
+        let items = vec![(5, 1, '-'), (9, 1, '|')];
+
+        assert_eq!(is_front_looping(entity, items), true);
+
+        let entity = (4, 1, 180);
+        let items = vec![(4, 2, '|'), (9, 1, '|')];
+
+        assert_eq!(is_front_looping(entity, items), true);
+
+        let entity = (4, 1, 270);
+        let items = vec![(3, 1, '-'), (9, 1, '-')];
+
+        assert_eq!(is_front_looping(entity, items), true);
     }
 }
