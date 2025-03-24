01cdafe6ec7533dd307daae4093aafd2ff0f4645
khairizalkhalid <khairizalkhalid@outlook.com>
2025-03-24 21:19:06 +0800

    remove mark_visited_with_direction because we should just store each tuple of entity xyd on each movement.

--- src/days/day6_part2.rs
+++ src/days/day6_part2.rs
@@ -1,36 +1,3 @@
-fn mark_visited_with_direction(
-    canvas: Vec<Vec<char>>,
-    entity_xy_dir: (i32, i32, i32),
-) -> Vec<Vec<char>> {
-    let (ntt_x, ntt_y, ntt_dir) = entity_xy_dir;
-
-    canvas
-        .into_iter()
-        .enumerate()
-        .map(|(row_i, row)| {
-            if row_i == ntt_y as usize {
-                row.into_iter()
-                    .enumerate()
-                    .map(|(col_i, col)| {
-                        if col_i == ntt_x as usize {
-                            match ntt_dir {
-                                0 => '|',
-                                90 => '-',
-                                180 => '|',
-                                270 => '-',
-                                _ => 'X',
-                            }
-                        } else {
-                            col
-                        }
-                    })
-                    .collect()
-            } else {
-                row
-            }
-        })
-        .collect()
-}
 
 fn is_front_looping(entity_xy_dir: (i32, i32, i32), items: Vec<(i32, i32, char)>) -> bool {
     let (ntt_x, ntt_y, ntt_dir) = entity_xy_dir;
@@ -86,62 +53,6 @@ pub fn run() {
 #[cfg(test)]
 mod test {
     use super::*;
-
-    #[test]
-    fn test_mark_visited_with_direction() {
-        let two_d_canvas = vec![
-            vec!['.', '.', '.', '.', '#', '.', '.', '.', '.', '.'],
-            vec!['.', '.', '.', '.', '.', '.', '.', '.', '.', '#'],
-        ];
-
-        let entity = (4, 1, 0);
-
-        let expected = vec![
-            vec!['.', '.', '.', '.', '#', '.', '.', '.', '.', '.'],
-            vec!['.', '.', '.', '.', '|', '.', '.', '.', '.', '#'],
-        ];
-
-        assert_eq!(
-            mark_visited_with_direction(two_d_canvas.clone(), entity),
-            expected
-        );
-
-        let entity = (4, 1, 90);
-
-        let expected = vec![
-            vec!['.', '.', '.', '.', '#', '.', '.', '.', '.', '.'],
-            vec!['.', '.', '.', '.', '-', '.', '.', '.', '.', '#'],
-        ];
-
-        assert_eq!(
-            mark_visited_with_direction(two_d_canvas.clone(), entity),
-            expected
-        );
-
-        let entity = (4, 1, 180);
-
-        let expected = vec![
-            vec!['.', '.', '.', '.', '#', '.', '.', '.', '.', '.'],
-            vec!['.', '.', '.', '.', '|', '.', '.', '.', '.', '#'],
-        ];
-
-        assert_eq!(
-            mark_visited_with_direction(two_d_canvas.clone(), entity),
-            expected
-        );
-
-        let entity = (4, 1, 270);
-
-        let expected = vec![
-            vec!['.', '.', '.', '.', '#', '.', '.', '.', '.', '.'],
-            vec!['.', '.', '.', '.', '-', '.', '.', '.', '.', '#'],
-        ];
-
-        assert_eq!(
-            mark_visited_with_direction(two_d_canvas.clone(), entity),
-            expected
-        );
-    }
 
     #[test]
     fn test_is_front_looping() {
