d743742a514c55b0f861a652c0e385197b1f2b96
khairizalkhalid <khairizalkhalid@outlook.com>
2025-03-25 22:07:18 +0800

    add test run from the day6 part two sample.

--- src/days/day6_part2.rs
+++ src/days/day6_part2.rs
@@ -43,6 +43,53 @@ fn is_front_looping(entity_xy_dir: (i32, i32, i32), items: &[(i32, i32, i32)]) -
     }
 }
 
+use super::day6_part1::get_item_coordinates;
+
+pub fn test_run() -> i32 {
+    let room_str = "....#.....\n.........#\n..........\n..#.......\n.......#..\n..........\n.#..^.....\n........#.\n#.........\n......#...";
+
+    let mut canvas = str_to_2d_canvas(room_str);
+    let obsticle = get_item_coordinates(canvas.clone(), '#');
+    let mut entity = get_entity_xy_dir(canvas.clone());
+
+    loop {
+        if !is_front_clear(entity, obsticle.clone()) {
+            entity = turn_right(entity);
+        } else if is_front_out_of_bounds(entity, canvas.clone()) {
+            break;
+        } else {
+            entity = move_forward(entity);
+            canvas = mark_visited(canvas, entity);
+        }
+    }
+
+    let marked = get_item_coordinates(canvas.clone(), 'X');
+    let canvas2 = str_to_2d_canvas(room_str);
+    let mut looping_marker: Vec<(i32, i32)> = vec![];
+
+    for m in marked {
+        let mut entity2 = get_entity_xy_dir(canvas2.clone());
+        let mut visited_with_direction: Vec<(i32, i32, i32)> = vec![];
+        let mut new_obs = obsticle.clone();
+        new_obs.push(m);
+        loop {
+            if is_front_looping(entity2, &visited_with_direction) {
+                looping_marker.push(m);
+                break;
+            } else if !is_front_clear(entity2, new_obs.clone()) {
+                entity2 = turn_right(entity2);
+            } else if is_front_out_of_bounds(entity2, canvas2.clone()) {
+                break;
+            } else {
+                visited_with_direction.push(entity2);
+                entity2 = move_forward(entity2);
+            }
+        }
+    }
+
+    looping_marker.iter().count() as i32
+}
+
 pub fn run() {
     println!("Day Six Part Two.");
     // for each of the mark visitted, add into a coordinate
@@ -79,5 +126,10 @@ mod test {
         let items = vec![(3, 1, 270), (9, 1, 0)];
 
         assert_eq!(is_front_looping(entity, &items), true);
+    }
+
+    #[test]
+    fn test_test_run() {
+        assert_eq!(test_run(), 6);
     }
 }
