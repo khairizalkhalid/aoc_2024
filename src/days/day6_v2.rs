a74d24e34781949a48f1f6f7464fb8e4b0e0245a
khairizalkhalid <khairizalkhalid@outlook.com>
2025-03-30 18:34:47 +0800

    add pseudocode for day6 version 2 for more optimize solution of day6.

---
+++ src/days/day6_v2.rs
@@ -0,0 +1,17 @@
+pub fn run() {
+    // recurr
+    // if obs found change ntt to tobs -1 +90
+    // get distance of ntt to tobs and add in steps
+    // no obs on path exit
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_main() {
+        let room_str = "....#.....\n.........#\n..........\n..#.......\n.......#..\n..........\n.#..^.....\n........#.\n#.........\n......#...";
+        assert_eq!(true, true);
+    }
+}
