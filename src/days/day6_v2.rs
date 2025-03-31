c078eba355e54ef3df9c72c57c2060b6cc56173f
khairizalkhalid <khairizalkhalid@outlook.com>
2025-03-31 23:28:19 +0800

    add get_distance_same_lime_target for day 6 v2

--- src/days/day6_v2.rs
+++ src/days/day6_v2.rs
@@ -1,3 +1,21 @@
+fn get_distance_same_line_target(entity: (i32, i32, i32), targets: Vec<(i32, i32)>) -> i32 {
+    let (x, y, dir) = entity;
+    let mut distance = 0;
+    for target in targets.iter() {
+        let (tx, ty) = *target;
+        if dir == 0 && tx == x && ty < y {
+            distance += y - ty;
+        } else if dir == 90 && tx > x && ty == y {
+            distance += tx - x;
+        } else if dir == 180 && tx == x && ty > y {
+            distance += ty - y;
+        } else if dir == 270 && tx < x && ty == y {
+            distance += x - tx;
+        }
+    }
+    distance
+}
+
 pub fn run() {
     // recurr
     // if obs found change ntt to tobs -1 +90
