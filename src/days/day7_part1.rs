41249675a2cb532a01c6f422d94a5d41f5180c83
khairizalkhalid <khairizalkhalid@outlook.com>
2025-04-27 22:03:21 +0800

    add day7 part1 task objective and sample test case.

---
+++ src/days/day7_part1.rs
@@ -0,0 +1,15 @@
+// test case:
+//
+// 190: 10 19
+// 3267: 81 40 27
+// 83: 17 5
+// 156: 15 6
+// 7290: 6 8 6 15
+// 161011: 16 10 13
+// 192: 17 8 14
+// 21037: 9 7 18 13
+// 292: 11 6 16 20
+//
+// task: in each line, find which one is correct if "x : a b c" where x must be equal a, b, c using
+// operators "+" and "*"
+// then add up all the one that is correct.
